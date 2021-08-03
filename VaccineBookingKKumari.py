5b23ee4cb423837300e612da1299d5e607929137
Jophine Antony <jophinep@gmail.com>
2021-08-03 16:32:21 +0530

    Updated to publish to SNS

--- VaccineBookingKKumari.py
+++ VaccineBookingKKumari.py
@@ -1,24 +1,20 @@
+"""
+Lambda function for vaccine crawler
+"""
+
+import os
 import time
+
+import boto3
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-import csv
-import threading
 
 SECONDS_TO_WAIT = 3
 URL = 'https://bookmyvaccine.kumaricovidcare.in'
-OUTPUT = 'result.csv'
-HEADERS = ['vaccine', 'qty', 'time']
-
-
-def notify(vaccine, qty):
-    print('Hurry up {} is available in {} qty'.format(vaccine, qty))
-
-
-def do_something(vaccine, qty):
-    threading.Thread(target=notify, args=(vaccine, qty)).start()
 
 
 def crawl():
+    """The actual crawler"""
     options = Options()
     options.headless = True
     browser = webdriver.Firefox(options=options)
@@ -28,26 +24,31 @@ def crawl():
 
     elements = browser.find_elements_by_xpath("//button[contains(@class,'availability-btn')]")
 
-    out = []
+    vaccine_data = []
 
     for e in elements:
         vaccine = e.find_element_by_xpath("preceding-sibling::p").text
         qty = 0 if e.text == 'Not Available' else int(e.text)
-        if qty > 0:
-            do_something(vaccine, qty)
+        vaccine_data.append({'vaccine_name': vaccine, 'quantity': qty, 'time': int(time.time())})
 
-        out.append({'vaccine': vaccine, 'qty': qty, 'time': int(time.time())})
+    return vaccine_data
 
-    return out
+def publish_result(vaccine_data):
+    """
+    Publish vaccine_data to SNS topic
+    """
+    topic_arn = os.environ["VACCINE_TOPIC"]
+    sns = boto3.resource('sns')
+    topic = sns.Topic(topic_arn)
+    message = "\n".join([
+        f"{vax['vaccine_name']}: {vax['quantity']} Available Now"
+        for vax in vaccine_data if vax['quantity'] > 0
+    ])
+    print("Vaccine message")
+    print(message)
+    if message:
+        topic.publish(Message=message)
 
-
-def store_result(result):
-    with open(OUTPUT, 'a') as file:
-        writer = csv.DictWriter(file, fieldnames=HEADERS)
-        for r in result:
-            writer.writerow(r)
-
-
-if __name__ == '__main__':
-    result = crawl()
-    store_result(result)
+def vaccine_crawler(*args):
+    vaccine_data = crawl()
+    publish_result(vaccine_data)
