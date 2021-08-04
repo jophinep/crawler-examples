cb2aaea52628c268ba98f86260fc6b35dca2fcda
Jophine Antony <jophinep@gmail.com>
2021-08-04 14:02:02 +0530

    Avoid multiple notifications

--- VaccineBookingKKumari.py
+++ VaccineBookingKKumari.py
@@ -4,6 +4,8 @@ Lambda function for vaccine crawler
 
 import os
 import time
+from datetime import date
+from glob import glob
 
 import boto3
 from selenium import webdriver
@@ -11,13 +13,39 @@ from selenium.webdriver.firefox.options import Options
 
 SECONDS_TO_WAIT = 3
 URL = 'https://bookmyvaccine.kumaricovidcare.in'
+TODAY = date.today()
+OUTFILE = f".vaccine_{TODAY}.txt"
+
+
+def get_old_files():
+    """Get old files"""
+    try:
+        files = []
+        files = glob(".vaccine_*.txt")
+        files.remove(OUTFILE)
+    except ValueError:
+        pass
+    return files
+
+def delete_old_files():
+    """Delete old files"""
+
+    deleted_count = 0
+    for filename in get_old_files():
+        try:
+            os.remove(filename)
+        except OSError:
+            print(f"Error deleting file {filename}")
+            continue
+        deleted_count += 1
+    print(f"{deleted_count} file{'s' if deleted_count != 1 else ''} deleted")
 
 
 def crawl():
     """The actual crawler"""
     options = Options()
     options.headless = True
-    browser = webdriver.Firefox(options=options)
+    browser = webdriver.Firefox(options=options, executable_path="./geckodriver")
     browser.get(URL)
 
     time.sleep(SECONDS_TO_WAIT)  # this need because results captured using ajax with jwt tokens
@@ -47,8 +75,18 @@ def publish_result(vaccine_data):
     print("Vaccine message")
     print(message)
     if message:
-        topic.publish(Message=message)
+        topic.publish(Message=f"Hurry Up!\n\n{message}")
+        with open(OUTFILE, "w") as outfile:
+            outfile.write(message)
 
 def vaccine_crawler(*args):
+    if os.path.isfile(OUTFILE):
+        print("Already notified today")
+        return
     vaccine_data = crawl()
     publish_result(vaccine_data)
+
+if __name__ == "__main__":
+    delete_old_files()
+    vaccine_crawler()
+
